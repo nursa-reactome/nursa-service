6125887426488ceff281a088790a017621b0ac82
FredLoney <loneyf@ohsu.edu>
2017-10-18 14:21:09 -0700

    Move test scripts from specs/ to src/test/groovy.

---
+++ src/test/groovy/org/reactome/server/nursa/AnalysisRestClientSpec.groovy
@@ -0,0 +1,18 @@
+package org.reactome.server.nursa
+
+import spock.lang.Specification
+
+
+/**
+ * @author Fred Loney <loneyf@ohsu.edu>
+ */
+class AnalysisRestClientSpec extends Specification {
+    def "performs analysis"() {
+        expect:
+        AnalysisRestClient.analyse(symbols).get("pathways").size() == count
+
+        where:
+          symbols                          | count
+        ["COL4A5", "IFI27"]                |   26
+    }
+}
