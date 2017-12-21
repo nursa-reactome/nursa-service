2297376a1a1e6000d76e2bc778f709a13e9e00ce
FredLoney <loneyf@ohsu.edu>
2017-12-21 15:08:03 -0800

    Scan for components.

--- src/main/java/org/reactome/nursa/service/Application.java
+++ src/main/java/org/reactome/nursa/service/Application.java
@@ -2,9 +2,12 @@ package org.reactome.nursa.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.reactome.nursa.service")
 public class Application {
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
