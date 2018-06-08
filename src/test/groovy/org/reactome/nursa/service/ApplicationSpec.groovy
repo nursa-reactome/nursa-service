7c913a24baa2894fc6f763d3f26e6e2a19f850f4
FredLoney <loneyf@ohsu.edu>
2018-06-07 18:09:43 -0700

    Work around integration test recognition bug by making the ApplicationSpec a unit test for now.

--- src/test/groovy/org/reactome/nursa/service/ApplicationSpec.groovy
+++ src/test/groovy/org/reactome/nursa/service/ApplicationSpec.groovy
@@ -7,18 +7,39 @@ import groovyx.net.http.RESTClient
 /**
  * @author Fred Loney <loneyf@ohsu.edu>
  */
-@Category(IntegrationTest.class)
+//
+// Note: See the pom.xml file for why this is falsely categorized
+// as a unit test for now.
+//@Category(IntegrationTest.class)
+@Category(UnitTest.class)
 class ApplicationSpec extends Specification {
     def client = new RESTClient('http://localhost:8484/')
 
-    def "fetches the dataset"() {
+    def "fetches the unexpanded cached dataset"() {
+        def (domain, dataset) = doi.split('/')
+        def fixture = new File("src/test/fixtures/${dataset}.json")
+        def cached = new File("/usr/local/reactomes/Reactome/production/" +
+                              "nursa/datasets/${domain}/${dataset}.json")
+        // Copy the fixture.
+        cached.newWriter().withWriter { f -> f << fixture.text }
         expect:
             def resp = client.get(path: 'dataset', query: [doi: doi])
-            assert resp.status == 200
-            assert resp.data.dataPoints.size() == count
+            assert resp.status == 200 : "Uncached dataset not fetched"
+            assert resp.data.experiments.size() == count
         where:
               doi                 | count
-            "10.1621/gTqItVnDEP"  |  255
+            "10.1621/gTqItVnDEP"  |  1
+    }
+
+    def "fetches the expanded cached dataset"() {
+        expect:
+             // Fetch cached.
+            def resp = client.get(path: 'dataset', query: [doi: doi])
+            assert resp.status == 200 : "Cached dataset not fetched"
+            assert resp.data.experiments.size() == count
+        where:
+              doi                 | count
+            "10.1621/gTqItVnDEP"  |  1
     }
 
     def "searches datasets based on a term"() {
